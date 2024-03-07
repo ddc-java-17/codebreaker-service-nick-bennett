60157ef6e41ed4a18f988177c6d00b132623b1ae
nick-bennett <nbennett10@cnm.edu>
2024-03-07 16:52:49 -0700

    RankingController started.

---
+++ src/main/java/edu/cnm/deepdive/codebreaker/controller/RankingController.java
@@ -0,0 +1,17 @@
+package edu.cnm.deepdive.codebreaker.controller;
+
+import edu.cnm.deepdive.codebreaker.service.AbstractRankingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RankingController {
+
+  private final AbstractRankingService rankingService;
+
+  @Autowired
+  public RankingController(AbstractRankingService rankingService) {
+    this.rankingService = rankingService;
+  }
+
+}
